8641bc33f48923e139b4aecc93e2f642a0df55ed
Cindy <1233wsh@gmail.com>
2018-12-23 00:22:42 +0900

    ~CH02.10

---
+++ understanding-ts/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+  </v-app>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+  };
+</script>
---
+++ understanding-ts/src/views/CH02/10_Numbers&Booleans.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+  </v-app>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+    mounted() {
+      // string
+      let myName = 'Max';
+      // myName = 28;
+
+      // number
+      let myAge = 27.5;
+      // myAge = 'Max';
+
+      // boolean
+      let hasHobbies = true;
+      // hasHobbies = 1;
+    },
+    methods: {
+    },
+  };
+</script>
